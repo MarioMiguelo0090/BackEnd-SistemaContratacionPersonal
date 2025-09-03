a786828be214d802a74ea9dce578d06c39a12758
Mario Limon <mariolimon132004@hotmail.com>
2025-09-03 09:13:24 -0600

    Servicios parcilamente realizado de acceso

--- API_REST/src/api_rest/controladores/AccesoControlador.js
+++ API_REST/src/api_rest/controladores/AccesoControlador.js
@@ -1,4 +1,4 @@
-import { ValidarInsercionAcceso } from "../esquemas/AccesoValidador.js";
+import { ValidarInsercionAcceso,ValidarEdicionParcialAcceso } from "../esquemas/AccesoValidador.js";
 
 export class AccesoControlador
 {
@@ -47,8 +47,7 @@ export class AccesoControlador
         }
         catch(error)
         {
-            //logger({mensaje:error});
-            console.error("Error real en RegistrarAcceso:", error);
+            //logger({mensaje:error});            
             res.status(500).json(
                 {
                     error: true,
@@ -59,4 +58,93 @@ export class AccesoControlador
         }        
     }
 
+    EditarAcceso = async (req,res) =>
+    {        
+        try
+        {
+            const idAcceso= parseInt(req.params['idAcceso']);
+            const {usuario, contrasenia,FKIdTipoAcceso,nombre,primerApellido,segundoApellido,estado}=req.body;
+            const Datos = {idAcceso,usuario, contrasenia,FKIdTipoAcceso,nombre,primerApellido,segundoApellido,estado};
+            const ResultadoValidacion = ValidarEdicionParcialAcceso(Datos);
+            if(ResultadoValidacion.success)
+            {
+                const ResultadoEdicion = await this.modeloAcceso.EditarAcceso({datos: ResultadoValidacion.data});
+                let resultadoEdicion = parseInt(ResultadoEdicion.estado);
+                if(resultadoEdicion === 500){
+                    res.status(resultadoEdicion).json(
+                        {
+                            error: true,
+                            estado: ResultadoEdicion.estado,
+                            mensaje: 'Ha ocurrido un error al intentar editar los datos de acceso'
+                        });
+                }
+                else
+                {
+                    res.status(resultadoEdicion).json(
+                        {
+                            error: resultadoEdicion !==200,
+                            estado: resultadoEdicion,
+                            mensaje: ResultadoEdicion.mensaje
+                        });
+                }
+            }
+            else
+            {
+                res.status(400).json({
+                    error: true,
+                    estado: 400,
+                    mensaje: 'Datos con formato inválido, por favor verifique los datos enviados.'
+                });
+            }
+        }
+        catch(error)
+        {
+            console.error("Error real en EditarAcceso:", error);
+            res.status(500).json(
+                {
+                    error: true,
+                    estado: 500,
+                    mensaje: 'Ha ocurrido un error al editar el acceso del usuario.'
+                });
+        }
+    }
+
+    BuscarUsuarioPorId = async (req, res) =>
+    {
+        try
+        {
+            const idAcceso = parseInt(req.params['idAcceso']);
+            const Datos = {idAcceso};
+            const ResultadoValidacion = ValidarEdicionParcialAcceso(Datos);
+            if(ResultadoValidacion.success)
+            {
+                const ResultadoConsulta = await this.modeloAcceso.BuscarUsuarioPorId({datos: ResultadoValidacion.data});
+                let resultadoConsulta = parseInt(ResultadoConsulta.estado);
+                res.status(resultadoConsulta).json({
+                    error: resultadoConsulta !==200,
+                    estado: resultadoConsulta,
+                    ...(resultadoConsulta === 200
+                        ? {usuario: ResultadoConsulta.usuarioEncontrado}
+                        : {mensaje: ResultadoConsulta.mensaje}                        
+                        )
+                });
+            }
+            else
+            {
+                res.status(400).json({
+                    error: true,
+                    estado: 400,
+                    mensaje: 'Datos con formato inválido, por favor verifique los datos enviados.'
+                });
+            }
+        }
+        catch(error)
+        {
+            res.status({
+                error: true,
+                estado: 500,
+                mensaje: "Ha ocurrido un error al querer buscar su usuario por ID"
+            });
+        }
+    }
 }
--- API_REST/src/api_rest/utilidades/Constantes.js
+++ API_REST/src/api_rest/utilidades/Constantes.js
@@ -2,5 +2,6 @@ export const MensajesAcceso = {
     REGISTRO_EXITOSO: { resultado: 200, mensaje: "Registro exitoso" },
     ERROR_DB: { resultado: 500, mensaje: "Ha ocurrido un error en la base de datos" },
     USUARIO_DUPLICADO: { resultado: 400, mensaje: "El nombre de usuario ya existe" },
-    ACTUALIZACION_EXITOSA: { resultado: 200, mensaje: "Actualización exitosa" }
+    ACTUALIZACION_EXITOSA: { resultado: 200, mensaje: "Actualización exitosa" },
+    USUARIO_PERDIDO: {resultado:404, mensaje:"No se ha encontrado el usuario"}
 };
