63600e271c714551d8cb3e98fcea3ffe802b0534
Mario Limon <mariolimon132004@hotmail.com>
2025-09-05 11:31:35 -0600

    Servicios implementadps parcialmente, falta jwt y logger

--- API_REST/src/api_rest/modelo/CedulaModelo.js
+++ API_REST/src/api_rest/modelo/CedulaModelo.js
@@ -1,6 +1,6 @@
 import sql from 'mssql';
 import { obtenerConexion } from './conexion/ConfiguracionConexion.js';
-import { MensajeGeneralesBD,MensajeCedula } from '../utilidades/Constantes.js';
+import { MensajeGeneralesBD,MensajeCedula, MensajeResultado } from '../utilidades/Constantes.js';
 
 export class ModeloCedula
 {
@@ -167,5 +167,243 @@ export class ModeloCedula
             }
         }
         return resultadoEdicion;   
+    }
+
+    static async ObtenerCedulaPorIdProceso({datos})
+    {
+        let resultadoConsulta;
+        let conexion;
+        try
+        {
+            conexion = await obtenerConexion();
+            const {FKIdProceso} = datos;
+            const Solicitud = await conexion.request()
+            .input('idProceso',sql.Int,FKIdProceso)
+            .execute('sp_ObtenerCedulaPorIdProceso');
+            const ResultadoQueryCedula = Solicitud.recordset;
+            if(ResultadoQueryCedula.length>0){
+                const cedulaConsultada=ResultadoQueryCedula[0];
+                if(cedulaConsultada.idCedula>0){
+                    resultadoConsulta = {estado: 200, cedula:ResultadoQueryCedula}
+                }else{
+                    resultadoConsulta = { estado: 500, mensaje: MensajeGeneralesBD.ERROR_DB };
+                }
+            }else {
+                resultadoConsulta = { estado: 404, mensaje: MensajeCedula.CEDULA_INEXISTENTE};
+            }
+        }catch (error) {
+            throw error;
+        } finally {
+            if (conexion) {
+                conexion.close(); 
+            }            
+        }    
+        return resultadoConsulta;    
+    }
+
+    static async ObtenerCompetenciasPorClasificacionCedula({datos})
+    {
+        let resultadoConsulta;
+        let conexion;
+        try
+        {
+            conexion = await obtenerConexion();
+            const {FKIdClasificacionCedula} = datos;
+            const Solicitud = await conexion.request()
+            .input('idClasificacionCedula',sql.Int,FKIdClasificacionCedula)
+            .execute('sp_ObtenerCompetenciasPorClasificacionCedula');
+            const ResultadoQueryCompetencias = Solicitud.recordset;
+            if(ResultadoQueryCompetencias.length>0){
+                const competencia=ResultadoQueryCompetencias[0];
+                if(competencia.idCompetencia>0){
+                    resultadoConsulta = {estado: 200, competencias:ResultadoQueryCompetencias}
+                }else{
+                    resultadoConsulta = { estado: 500, mensaje: MensajeGeneralesBD.ERROR_DB };
+                }
+            }else {
+                resultadoConsulta = { estado: 404, mensaje: MensajeCedula.COMPETENCIA_INEXISTENTE};
+            }
+        }catch (error) {
+            throw error;
+        } finally {
+            if (conexion) {
+                conexion.close(); 
+            }            
+        }    
+        return resultadoConsulta;    
+    }
+
+    static async InsertarNuevoResultado({datos})
+    {
+        let resultadoInsercion;
+        let conexion;
+        try
+        {
+            conexion = await obtenerConexion();
+            const {
+                FKIdCedula,
+                psicometriaComunicacion,
+                psicometriaTrabajoEnEquipo,
+                psicometriaOrientacionAlServicio,
+                psicometriaSensibilidadALineamientos,
+                psicometriaPlaneacionOrganizacion,
+                psicometriaAnalisisProblemas,
+                psicometriaEnfoqueResultados,
+                psicometriaControlActividades,
+                psicometriaEnfoqueCalidad,
+                psicometriaRelacionesInterpersonales,
+                psicometriaLiderazgo,
+                psicometriaTomaDecisiones,
+                psicometriaDinamismo,
+                psicometriaInnovacion,
+                psicometriaPensamientoEstrategico,
+                psicometriaNegociacion
+            } = datos;
+            const Solicitud = await conexion.request()
+            .input('FKIdCedula', sql.Int, FKIdCedula)
+            .input('psicometriaComunicacion', sql.Float, psicometriaComunicacion)
+            .input('psicometriaTrabajoEnEquipo', sql.Float, psicometriaTrabajoEnEquipo)
+            .input('psicometriaOrientacionAlServicio', sql.Float, psicometriaOrientacionAlServicio)
+            .input('psicometriaSensibilidadALineamientos', sql.Float, psicometriaSensibilidadALineamientos)
+            .input('psicometriaPlaneacionOrganizacion', sql.Float, psicometriaPlaneacionOrganizacion)
+            .input('psicometriaAnalisisProblemas', sql.Float, psicometriaAnalisisProblemas)
+            .input('psicometriaEnfoqueResultados', sql.Float, psicometriaEnfoqueResultados)
+            .input('psicometriaControlActividades', sql.Float, psicometriaControlActividades)
+            .input('psicometriaEnfoqueCalidad', sql.Float, psicometriaEnfoqueCalidad)
+            .input('psicometriaRelacionesInterpersonales', sql.Float, psicometriaRelacionesInterpersonales)
+            .input('psicometriaLiderazgo', sql.Float, psicometriaLiderazgo)
+            .input('psicometriaTomaDecisiones', sql.Float, psicometriaTomaDecisiones)
+            .input('psicometriaDinamismo', sql.Float, psicometriaDinamismo)
+            .input('psicometriaInnovacion', sql.Float, psicometriaInnovacion)
+            .input('psicometriaPensamientoEstrategico', sql.Float, psicometriaPensamientoEstrategico)
+            .input('psicometriaNegociacion', sql.Float, psicometriaNegociacion)
+            .execute('sp_RegistrarResultado');
+            const Resultado = Solicitud.recordset;
+            if(Resultado.length>0){
+                const Registro = Resultado[0];
+                if(Registro.idResultado > 0){
+                    resultadoInsercion ={
+                        ...MensajeResultado.REGISTRO_EXITOSO,
+                        estado: MensajeResultado.REGISTRO_EXITOSO.resultado
+                    };
+                }else{
+                    resultadoInsercion = {
+                        ...MensajeResultado.RESULTADO_INEXISTENTE,
+                        estado: MensajeResultado.RESULTADO_INEXISTENTE.resultado
+                    };
+                }
+            }
+        }catch (error) {
+            throw error;
+        } finally {
+            if (conexion) {
+                conexion.close(); 
+            }            
+        }
+        return resultadoInsercion;
+    }
+
+    static async EditarResultadoExistente({datos})
+    {
+        let resultadoEdicion;
+        let conexion;
+        try
+        {
+            conexion = await obtenerConexion();
+            const {
+                idResultado,
+                psicometriaComunicacion,
+                psicometriaTrabajoEnEquipo,
+                psicometriaOrientacionAlServicio,
+                psicometriaSensibilidadALineamientos,
+                psicometriaPlaneacionOrganizacion,
+                psicometriaAnalisisProblemas,
+                psicometriaEnfoqueResultados,
+                psicometriaControlActividades,
+                psicometriaEnfoqueCalidad,
+                psicometriaRelacionesInterpersonales,
+                psicometriaLiderazgo,
+                psicometriaTomaDecisiones,
+                psicometriaDinamismo,
+                psicometriaInnovacion,
+                psicometriaPensamientoEstrategico,
+                psicometriaNegociacion
+            } = datos;
+            const Solicitud = await conexion.request()
+            .input('idResultado', sql.Int, idResultado)
+            .input('psicometriaComunicacion', sql.Float, psicometriaComunicacion)
+            .input('psicometriaTrabajoEnEquipo', sql.Float, psicometriaTrabajoEnEquipo)
+            .input('psicometriaOrientacionAlServicio', sql.Float, psicometriaOrientacionAlServicio)
+            .input('psicometriaSensibilidadALineamientos', sql.Float, psicometriaSensibilidadALineamientos)
+            .input('psicometriaPlaneacionOrganizacion', sql.Float, psicometriaPlaneacionOrganizacion)
+            .input('psicometriaAnalisisProblemas', sql.Float, psicometriaAnalisisProblemas)
+            .input('psicometriaEnfoqueResultados', sql.Float, psicometriaEnfoqueResultados)
+            .input('psicometriaControlActividades', sql.Float, psicometriaControlActividades)
+            .input('psicometriaEnfoqueCalidad', sql.Float, psicometriaEnfoqueCalidad)
+            .input('psicometriaRelacionesInterpersonales', sql.Float, psicometriaRelacionesInterpersonales)
+            .input('psicometriaLiderazgo', sql.Float, psicometriaLiderazgo)
+            .input('psicometriaTomaDecisiones', sql.Float, psicometriaTomaDecisiones)
+            .input('psicometriaDinamismo', sql.Float, psicometriaDinamismo)
+            .input('psicometriaInnovacion', sql.Float, psicometriaInnovacion)
+            .input('psicometriaPensamientoEstrategico', sql.Float, psicometriaPensamientoEstrategico)
+            .input('psicometriaNegociacion', sql.Float, psicometriaNegociacion)
+            .execute('sp_ActualizarResultado');
+            const ResultadoSP = Solicitud.recordset[0]?.actualizado;
+            if(ResultadoSP === 1){
+                resultadoEdicion = {
+                    ...MensajeCedula.ACTUALIZACION_EXITOSA,
+                    estado: MensajeCedula.ACTUALIZACION_EXITOSA.resultado
+                };
+            }else if (ResultadoSP === 0){
+                resultadoEdicion = {
+                    ...MensajeResultado.RESULTADO_INEXISTENTE,
+                    estado: MensajeResultado.RESULTADO_INEXISTENTE.resultado
+                };
+            }else {
+                resultadoEdicion={
+                    ...MensajeGeneralesBD.ERROR_DB,
+                    estado: MensajeGeneralesBD.ERROR_DB.resultado
+                };
+            }
+        }catch (error) {
+            throw error;
+        } finally {
+            if (conexion) {
+                conexion.close(); 
+            }            
+        }
+        return resultadoEdicion;
+    }
+
+    static async ObtenerResultadosPorIdCedula({datos})
+    {
+        let resultadoConsulta;
+        let conexion;
+        try
+        {
+            conexion = await obtenerConexion();
+            const {FKIdCedula} = datos;
+            const Solicitud = await conexion.request()
+            .input('idCedula',sql.Int,FKIdCedula)
+            .execute('sp_ObtenerResultadosPorIdCedula');
+            const ResultadoQueryResultado = Solicitud.recordset;
+            if(ResultadoQueryResultado.length>0){
+                const resultadoConsultado=ResultadoQueryResultado[0];
+                if(resultadoConsultado.idResultado>0){
+                    resultadoConsulta = {estado: 200, resultados:ResultadoQueryResultado}
+                }else{
+                    resultadoConsulta = { estado: 500, mensaje: MensajeGeneralesBD.ERROR_DB };
+                }
+            }else {
+                resultadoConsulta = { estado: 404, mensaje: MensajeResultado.RESULTADO_INEXISTENTE};
+            }
+        }catch (error) {
+            throw error;
+        } finally {
+            if (conexion) {
+                conexion.close(); 
+            }            
+        }
+        return resultadoConsulta;
     }
 }
