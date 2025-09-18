f379cf8de8c16c49dd2476a127ae628138d5c8c6
Mario Limon <mariolimon132004@hotmail.com>
2025-09-18 12:29:31 -0600

    Obtencion de Analistas

--- API_REST/src/api_rest/controladores/AccesoControlador.js
+++ API_REST/src/api_rest/controladores/AccesoControlador.js
@@ -317,4 +317,29 @@ export class AccesoControlador
             });
         }
     }
+
+    ObtenerTodosAnalistas = async(req,res) =>
+    {
+        try
+        {
+            const ResultadoConsulta = await this.modeloAcceso.ObtenerTodosLosAnalistas();
+            let resultadoConsulta = parseInt(ResultadoConsulta.estado);
+            res.status(resultadoConsulta).json({
+                error: resultadoConsulta !== 200,
+                estado: resultadoConsulta,
+                ...(resultadoConsulta === 200
+                    ? {usuarios: ResultadoConsulta.usuarios}
+                    : {mensaje: ResultadoConsulta.mensaje}
+                )
+            });            
+        }catch(error)
+        {
+            logger({mensaje:error});
+            res.status({
+                error: true,
+                estado: 500,
+                mensaje: "Ha ocurrido un error en el servidor"
+            });
+        }
+    }
 }
--- API_REST/src/api_rest/modelo/AccesoModelo.js
+++ API_REST/src/api_rest/modelo/AccesoModelo.js
@@ -280,4 +280,28 @@ export class ModeloAcceso{
         return resultadoConsulta;
     }
     
+    static async ObtenerTodosLosAnalistas()
+    {
+        let resultadoConsulta;
+        let conexion;
+        try
+        {
+            conexion = await obtenerConexion();
+            const Solicitud = await conexion.request()
+            .execute('ObtenerAccesosAnalistas');
+            const usuarios = Solicitud.recordset;
+            if(usuarios.length > 0){
+                resultadoConsulta = {estado: 200, usuarios};
+            }else{
+                resultadoConsulta = {estado: 400, mensaje: MensajesAcceso.USUARIO_PERDIDO};
+            }
+        }catch (error) {
+            throw error;
+        } finally {
+            if (conexion) {
+                conexion.close();
+            }
+        } 
+        return resultadoConsulta;
+    }
 }
