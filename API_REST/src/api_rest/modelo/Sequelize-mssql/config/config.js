86a62be4f3df96012b7a7c0d5ab9e206019769f3
chris970712 <chrisvasquez985@gmail.com>
2026-03-04 10:52:46 -0600

    Configuracion cadenas de conexion y roles de permiso

--- API_REST/src/api_rest/modelo/Sequelize-mssql/config/config.js
+++ API_REST/src/api_rest/modelo/Sequelize-mssql/config/config.js
@@ -22,36 +22,36 @@ export const sequelize = new Sequelize(
     
 export const sequelizeAdministrador = new Sequelize(
     process.env.BD_BASEDEDATOS,
-    process.env.BD_USUARIO,
-    process.env.BD_CONTRASENIA,
+    process.env.BD_USUARIOADMINISTRADOR,
+    process.env.BD_USUARIOADMINISTRADORPASS,
     {host: process.env.BD_SERVIDOR, port: process.env.BD_PUERTO, ...opciones}
 );
 
 export const sequelizeAnalista = new Sequelize(
     process.env.BD_BASEDEDATOS,
-    process.env.BD_USUARIO,
-    process.env.BD_CONTRASENIA,
+    process.env.BD_USUARIOANALISTA,
+    process.env.BD_USUARIOANALISTAPASS,
     {host: process.env.BD_SERVIDOR, port: process.env.BD_PUERTO, ...opciones}
 );
 
 export const sequelizeGestorDeSolicitudes = new Sequelize(
     process.env.BD_BASEDEDATOS,
-    process.env.BD_USUARIO,
-    process.env.BD_CONTRASENIA,
+    process.env.BD_USUARIOGESTORSOLICITUDES,
+    process.env.BD_USUARIOGESTORSOLICITUDESPASS,
     {host: process.env.BD_SERVIDOR, port: process.env.BD_PUERTO, ...opciones}
 );
 
 export const sequelizeJefeDeDepartamento = new Sequelize(
     process.env.BD_BASEDEDATOS,
-    process.env.BD_USUARIO,
-    process.env.BD_CONTRASENIA,
+    process.env.BD_USUARIOJEFEDEPARTAMENTO,
+    process.env.BD_USUARIOJEFEDEPARTAMENTOPASS,
     {host: process.env.BD_SERVIDOR, port: process.env.BD_PUERTO, ...opciones}
 );
 
 export const sequelizeInicioDeSesion= new Sequelize(
     process.env.BD_BASEDEDATOS,
-    process.env.BD_USUARIO,
-    process.env.BD_CONTRASENIA,
+    process.env.BD_USUARIOLOGIN,
+    process.env.BD_USUARIOLOGINPASS,
     {host: process.env.BD_SERVIDOR, port: process.env.BD_PUERTO, ...opciones}
 );
 
